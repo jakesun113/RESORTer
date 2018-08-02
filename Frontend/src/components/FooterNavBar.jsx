c66df8b18e60ceba6bb9f622651c78bc65c7902a
Ruoyi Wang <ruoyiw@student.unimelb.edu.au>
2018-08-02 10:14:17 +1000

    1.5-footer part (term of use, privacy statement), 1.6-back to top button

--- Frontend/src/components/FooterNavBar.jsx
+++ Frontend/src/components/FooterNavBar.jsx
@@ -1,66 +1,52 @@
-import React, { Component } from "react";
-import BackTopBtn from "./BackTopBtn";
-// style
-const shortLineStyle = {
-  marginTop: "50px",
-  width: "90%"
-};
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/Footer.css';
+import { Link } from 'react-router-dom'
+import BackTopBtn from './BackTopBtn';
 
-const upperTextStyle = {
-  textDecoration: "underline",
-  color: "black"
-};
 
-const marginLeftStyle = {
-  paddingRight: "100px",
-  paddingLeft: "250px"
-};
+export default class Footer extends React.Component {
 
-const marginRightStyle = {
-  paddingRight: "15px",
-  paddingLeft: "350px"
-};
+    render() {
 
-class FooterNavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <div className="container">
-          <div className="row">
-            <div style={marginRightStyle}>
-              <a style={upperTextStyle} href="">
-                Term of Use
-              </a>
+        return (
+          <footer class="page-footer font-small">
+            
+            <div class="container text-md-center py-3">
+            <div class="row no-gutters">
+               <div class="col-md-6 mt-md-0 mt-3">
+                 <Link class="terms" to={`/terms`}>Terms of Use</Link>
+               </div>
+            
+               <hr class="clearfix w-100 d-md-none pb-3" />
+            
+               <div class="col-md-6 mb-md-0 mb-3">
+                <Link class="privacy" to={`/privacy-statement`}>Privacy Statement</Link>
+               </div>
+               <hr/>
             </div>
-            <div style={marginLeftStyle}>
-              <a style={upperTextStyle} href="">
-                Privacy Statement
-              </a>
-            </div>
-          </div>
-          <hr style={shortLineStyle} />
-          <div className="row">
-            <div className="col">
-              <span>Melbourne, Australia</span>
-            </div>
-            <div className="col">
-              <a href="">info@resorter.app</a>
-            </div>
-            <div className="col">
-              <span>Copyright 2017 - RESORTer - All Rights Reserved</span>
-            </div>
-          </div>
-        </div>
 
-        {this.props.isHidden !== 1 ? (
-          <BackTopBtn scrollStepInPx="50" delayInMs="16.66" />
-        ) : (
-          ""
-        )}
-      </React.Fragment>
-    );
-  }
+            <hr/>
+            <div class="footer-copyright">
+            <div class="row">
+               <div class="col-md-3 mx-auto">
+                 Melbourne, Australia
+               </div>
+               <div class="col-md-4 mx-auto">
+                 <a href="mailto:info@resorter.app">info@resorter.app</a>
+               </div>
+               <div class="col-md-5 mx-auto">
+                  Copyright 2017 - RESORTer - All Rights Reserved
+               </div>
+            </div>
+            </div>
+            </div>
+            {this.props.isHidden !== 1 ? (
+            <BackTopBtn scrollStepInPx="50" delayInMs="16.66" />
+            ) : (
+            ""
+            )}
+          </footer>   
+        )
+    }
 }
-
-export default FooterNavBar;
