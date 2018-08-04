25fbf2d765654f8064e1b79cda4747893ef2b335
yunjie Jia <453773874@qq.com>
2018-08-04 22:54:25 +1000

    delete useless code

--- Frontend/src/pages/App.jsx
+++ Frontend/src/pages/App.jsx
@@ -3,26 +3,10 @@ import NavBar from "../components/NavBar";
 import MainBody from "./MainBody";
 import FooterNavBar from "../components/FooterNavBar";
 class App extends Component {
-  state = {
-    user: "",
-    redirect: false,
-    login: false
-  };
-  componentDidMount() {
-    if (sessionStorage.getItem("userSocialData") || this.state.redirect) {
-      let data = JSON.parse(sessionStorage.getItem("userSocialData"));
-      this.setState({
-        user: data.name,
-        login: true
-      });
-    }
-  }
-
   render() {
     return (
       <div>
-        {/* <NavBar /> */}
-        <NavBar login={this.state.login} user={this.state.user} />
+        <NavBar />
         <MainBody />
         <FooterNavBar />
       </div>
