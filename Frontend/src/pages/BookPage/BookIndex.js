db459ad9124b097b42343a4e33a4a888b79a1fd1
SeaskyHo <hehaitian@SeaskyMAC.local>
2018-09-19 10:24:07 +1000

    ss

--- Frontend/src/pages/BookPage/BookIndex.js
+++ Frontend/src/pages/BookPage/BookIndex.js
@@ -21,18 +21,15 @@ class BookIndex extends Component {
                     <Switch>
                         <Route
                             path={`/booking/${place}/who`}
-                            render={() => <BookTripWhoPage place={place} masterID={masterID} tripID={tripID}
-                                                           history={history}/>}
+                            render={() => <BookTripWhoPage place={place} history={history}/>}
                         />
                         <Route
                             path={`/booking/${place}/sleep`}
-                            render={() => <BookingAccommodation place={place} masterID={masterID} tripID={tripID}
-                                                                history={history}/>}
+                            render={() => <BookingAccommodation place={place} history={history}/>}
                         />
                         <Route
                             path={`/booking/${place}/doing`}
-                            render={() => <BookingActivity place={place} masterID={masterID} tripID={tripID}
-                                                           history={history}/>}
+                            render={() => <BookingActivity place={place} history={history}/>}
                         />
                     </Switch>
                 </div>
