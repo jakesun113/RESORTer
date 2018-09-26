92e53ef7efb9b98871ef19fe0a6c057f6853b024
yunjie Jia <453773874@qq.com>
2018-09-26 16:18:47 +1000

    fix responsive

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -264,32 +264,34 @@ class Navbar extends Component {
         <div className="container-fluid">
           <nav>
             {/* up */}
-            <div className="row">
-              <div className="col-xs-0 col-sm-0 col-lg-1" />
+            <div className="row" style={{ textAlign: "center" }}>
+              <div className="col-lg-3" />
               {/* coming soon logo */}
-              <div className="col-xs-12 col-sm-4 col-lg-3 logo_coming_soon">
-                <span>
-                  <img
-                    src="https://static.wixstatic.com/media/25b4a3_0a86277c361e458298291ef1d9ed0ba8~mv2.png/v1/fill/w_200,h_200,al_c,usm_0.66_1.00_0.01/25b4a3_0a86277c361e458298291ef1d9ed0ba8~mv2.png"
-                    alt=""
-                  />
-                </span>
+              <div
+                className="col-12 col-sm-12 col-md-12 col-lg-2 logo_coming_soon"
+                style={{ textAlign: "center" }}
+              >
+                <img
+                  src="https://static.wixstatic.com/media/25b4a3_0a86277c361e458298291ef1d9ed0ba8~mv2.png/v1/fill/w_200,h_200,al_c,usm_0.66_1.00_0.01/25b4a3_0a86277c361e458298291ef1d9ed0ba8~mv2.png"
+                  alt=""
+                />
               </div>
               {/* company logo */}
-              <div className="col-xs-12 col-sm-4 col-lg-3 logo_front">
-                <span>
-                  <img
-                    src="https://static.wixstatic.com/media/25b4a3_476f364fc74b4d3fb6c657519d3c90d2~mv2.png/v1/fill/w_366,h_156,al_c,usm_0.66_1.00_0.01/25b4a3_476f364fc74b4d3fb6c657519d3c90d2~mv2.png"
-                    alt=""
-                  />
-                </span>
+              <div
+                className="col-12 col-sm-12 col-md-12 col-lg-2 logo_front"
+                style={{ textAlign: "center" }}
+              >
+                <img
+                  src="https://static.wixstatic.com/media/25b4a3_476f364fc74b4d3fb6c657519d3c90d2~mv2.png/v1/fill/w_366,h_156,al_c,usm_0.66_1.00_0.01/25b4a3_476f364fc74b4d3fb6c657519d3c90d2~mv2.png"
+                  alt=""
+                />
               </div>
               {/* continue book trip area */}
-              <div className="col-12 col-sm-3 col-lg-3">
+              <div className="col-12 col-sm-12 col-md-12 col-lg-3">
                 <ContinueBookTrip />
               </div>
               {/* admin btn */}
-              <div className="col-xs-12 col-sm-4 col-sm-1 col-lg-1 button_admin">
+              <div className="col-12 col-sm-12 col-md-12 col-lg-1 button_admin">
                 <a className="navbar-brand" href="/">
                   <img
                     src="https://static.wixstatic.com/media/25b4a3_fae0b5a09c5c4a4cbd36b211a9075836~mv2.png/v1/fill/w_66,h_66,al_c,lg_1/25b4a3_fae0b5a09c5c4a4cbd36b211a9075836~mv2.png"
@@ -405,7 +407,8 @@ class Navbar extends Component {
                   <div className="row">
                     <div className="col-xl-2 col-lg-2" />
                     <div className="col-xl-2 col-lg-2 userBtn">
-                      <Link to=""
+                      <Link
+                        to=""
                         onClick={() => {
                           this.setState({ isShowLoginWindow: true });
                         }}
