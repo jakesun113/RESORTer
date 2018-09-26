e871b2e833d50c2ca8459638640afaa7ec755ef0
yunjie Jia <453773874@qq.com>
2018-09-26 15:38:20 +1000

    fix most search

--- Frontend/src/components/template/ImageCard.jsx
+++ Frontend/src/components/template/ImageCard.jsx
@@ -1,178 +1,188 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../../css/template/ImageCard.css";
-import axios from "axios"
-import {withCookies, Cookies} from 'react-cookie';
-import {instanceOf} from 'prop-types';
-
+import axios from "axios";
+import { withCookies, Cookies } from "react-cookie";
+import { instanceOf } from "prop-types";
+import SmallEllipseBtn from "./SmallEllipseBtn";
+import styled from "styled-components";
 const CardSubTitleStyle = {
-    color: "#2ab4ff"
+  color: "#2ab4ff"
 };
+
+const StyledImage = styled.img`
+  width: 100% !important;
+  height: 250px !important;
+  object-fit: cover;
+`;
+
 //TODO: Do not judge the condition of token in this page.
 class ImageCard extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
 
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired
-    };
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleBook = this.handleBook.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.handleBook = this.handleBook.bind(this);
-    }
+  async handleBook() {
+    //only handle login with email user
+    if (
+      sessionStorage.getItem("userSocialData") &&
+      JSON.parse(sessionStorage.getItem("userSocialData")).provider === "email"
+    ) {
+      let BaseURL = "http://127.0.0.1:3333/api/";
+      let postData;
+      postData = {
+        token: JSON.parse(sessionStorage.getItem("userToken")).token
+      };
+      await axios.post(BaseURL + "check-token", postData).then(response => {
+        //handle token is not valid, return to login
+        if (response.data.tokenValid === false) {
+          console.log("token expired");
 
-    async handleBook() {
+          this.handleLogout();
 
-        //only handle login with email user
-        if (sessionStorage.getItem('userSocialData') &&
-            JSON.parse(sessionStorage.getItem('userSocialData')).provider === 'email') {
-            let BaseURL = "http://127.0.0.1:3333/api/";
-            let postData;
-            postData = {
-                token: JSON.parse(sessionStorage.getItem('userToken')).token
-            };
-            await axios.post(BaseURL + "check-token", postData).then(response => {
+          this.props.history.push({
+            pathname: "/login"
+          });
+        }
+        //token is valid
+        else {
+          console.log("token valid");
+          //save token into session
+          let sessionData;
+          sessionData = {
+            token: response.data.token
+          };
+          sessionStorage.setItem("userToken", JSON.stringify(sessionData));
 
-                //handle token is not valid, return to login
-                if (response.data.tokenValid === false) {
-                    console.log("token expired");
+          //save token into cookie
 
-                    this.handleLogout();
+          let date = new Date();
+          date.setTime(date.getTime() + +2592000);
+          const { cookies } = this.props;
 
-                    this.props.history.push({
-                        pathname: '/login'
-                    })
-                }
-                //token is valid
-                else {
-                    console.log("token valid");
-                    //save token into session
-                    let sessionData;
-                    sessionData = {
-                        token: response.data.token
-                    };
-                    sessionStorage.setItem("userToken", JSON.stringify(sessionData));
+          //only when user click "remember me", update the token in cookies
+          if (cookies.get("access-token")) {
+            cookies.set("access-token", response.data.token, {
+              expires: date,
+              path: "/"
+            });
 
-                    //save token into cookie
+            console.log(
+              "token has been extended. Token is: " +
+                cookies.get("access-token")
+            );
+          }
 
-                    let date = new Date();
-                    date.setTime(date.getTime() + +2592000);
-                    const {cookies} = this.props;
+          //Jump into book page
+          let postData = {};
+          postData.resortName = this.props.title;
+          postData.token = JSON.parse(
+            sessionStorage.getItem("userToken")
+          ).token;
 
-                    //only when user click "remember me", update the token in cookies
-                    if (cookies.get("access-token")) {
-                        cookies.set("access-token", response.data.token, {
-                            expires: date,
-                            path: "/"
-                        });
-
-                        console.log(
-                            "token has been extended. Token is: " +
-                            cookies.get("access-token")
-                        );
-                    }
-
-                    //Jump into book page
-                    let postData = {};
-                    postData.resortName = this.props.title;
-                    postData.token = JSON.parse(sessionStorage.getItem('userToken')).token;
-
-                    axios.post("http://127.0.0.1:3333/api/enrollTrip", postData)
-                        .then(response => {
-                            if (response.data.status === 'success') {
-
-                                this.props.history.push({
-                                    pathname: `/booking/${this.props.title}/who`,
-                                    state: {
-                                        masterID: response.data.masterID,
-                                        resortID: response.data.resortID,
-                                        tripID: response.data.tripID
-                                    }
-                                })
-
-                            } else {
-                                alert('SERVER ERROR: Please try again :)')
-                            }
-                        })
-                }
+          axios
+            .post("http://127.0.0.1:3333/api/enrollTrip", postData)
+            .then(response => {
+              if (response.data.status === "success") {
+                this.props.history.push({
+                  pathname: `/booking/${this.props.title}/who`,
+                  state: {
+                    masterID: response.data.masterID,
+                    resortID: response.data.resortID,
+                    tripID: response.data.tripID
+                  }
+                });
+              } else {
+                alert("SERVER ERROR: Please try again :)");
+              }
             });
         }
-        //facebook&google login
-        else if (sessionStorage.getItem('userSocialData') &&
-            JSON.parse(sessionStorage.getItem('userSocialData')).provider !== 'email') {
-            //Jump into book page
-            let postData = {};
-            postData.resortName = this.props.title;
-            postData.token = JSON.parse(sessionStorage.getItem('userToken')).token;
-
-            await axios.post("http://127.0.0.1:3333/api/enrollTrip", postData)
-                .then(response => {
-                    if (response.data.status === 'success') {
-
-                        this.props.history.push({
-                            pathname: `/booking/${this.props.title}/who`,
-                            state: {
-                                masterID: response.data.masterID,
-                                resortID: response.data.resortID,
-                                tripID: response.data.tripID
-                            }
-
-                        })
-
-                    } else {
-                        alert('SERVER ERROR: Please try again :)')
-                    }
-                })
-            //logout status
-        } else if (!sessionStorage.getItem('userSocialData')) {
-            this.props.history.push({
-                pathname: '/login'
-            })
-        }
-    };
-
-    handleLogout = () => {
-        const {cookies} = this.props;
-
-        sessionStorage.removeItem("userSocialData");
-        sessionStorage.removeItem("userToken");
-        sessionStorage.removeItem("userImage");
-        sessionStorage.removeItem("userFinishProfile");
-        cookies.remove("user-name");
-        cookies.remove("access-token");
-        cookies.remove("user-pic");
-        cookies.remove("user-provider");
-        cookies.remove("user-profileFinished");
-    };
-
-    render() {
-
-        return (
-            <React.Fragment>
-                <div className="card h-100" style={{width: "20rem"}}>
-                    <img
-                        className="card-img-top"
-                        src={this.props.imgSrc}
-                        alt="Card cap"
-                    />
-                    <div className="card-body">
-                        <h5 className="card-title">{this.props.title}</h5>
-                        <p style={CardSubTitleStyle}>{this.props.subTitle}</p>
-                        <p className="card-text card-body-size">{this.props.text}</p>
-                        <div className="botton_right">
-                            <a
-                                href={`/booking/${this.props.title}/who`}
-                                className="btn btn-primary"
-                                // onMouseEnter={this.handleAuth}
-                                // onClick={this.handleBook}
-                            >
-                                <span>{this.props.btnText}</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
+      });
     }
+    //facebook&google login
+    else if (
+      sessionStorage.getItem("userSocialData") &&
+      JSON.parse(sessionStorage.getItem("userSocialData")).provider !== "email"
+    ) {
+      //Jump into book page
+      let postData = {};
+      postData.resortName = this.props.title;
+      postData.token = JSON.parse(sessionStorage.getItem("userToken")).token;
+
+      await axios
+        .post("http://127.0.0.1:3333/api/enrollTrip", postData)
+        .then(response => {
+          if (response.data.status === "success") {
+            this.props.history.push({
+              pathname: `/booking/${this.props.title}/who`,
+              state: {
+                masterID: response.data.masterID,
+                resortID: response.data.resortID,
+                tripID: response.data.tripID
+              }
+            });
+          } else {
+            alert("SERVER ERROR: Please try again :)");
+          }
+        });
+      //logout status
+    } else if (!sessionStorage.getItem("userSocialData")) {
+      this.props.history.push({
+        pathname: "/login"
+      });
+    }
+  }
+
+  handleLogout = () => {
+    const { cookies } = this.props;
+
+    sessionStorage.removeItem("userSocialData");
+    sessionStorage.removeItem("userToken");
+    sessionStorage.removeItem("userImage");
+    sessionStorage.removeItem("userFinishProfile");
+    cookies.remove("user-name");
+    cookies.remove("access-token");
+    cookies.remove("user-pic");
+    cookies.remove("user-provider");
+    cookies.remove("user-profileFinished");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="card" style={{ width: "90%" }}>
+          <StyledImage
+            className="card-img-top"
+            src={this.props.imgSrc}
+            alt="Card image cap"
+          />
+          <div className="card-body h-75">
+            <h5 className="card-title">{this.props.title}</h5>
+            <p style={CardSubTitleStyle}>{this.props.subTitle}</p>
+            <p className="card-text card-body-size">{this.props.text}</p>
+            <div className="botton_right">
+              <a
+                href={`/booking/${this.props.title}/who`}
+                className="btn btn-primary"
+                // onMouseEnter={this.handleAuth}
+                // onClick={this.handleBook}
+              >
+                <SmallEllipseBtn
+                  text={this.props.btnText}
+                  btnColor="orangered"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default withCookies(ImageCard);
