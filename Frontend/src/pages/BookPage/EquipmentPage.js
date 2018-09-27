68a0c7dc79a345365d6328f972d656e6aa909a52
yunjie Jia <453773874@qq.com>
2018-09-27 15:04:19 +1000

    add equipment page

---
+++ Frontend/src/pages/BookPage/EquipmentPage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Equipmentpage extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <br />
+          {/* title */}
+          <div
+            className="row"
+            style={{ color: "#4682B4", fontSize: "26px", fontWeight: "bold" }}
+          >
+            <div className="col-1" />
+            <div className="col-4"> 4. EQUIPMENT?</div>
+            <div className="col-7" />
+          </div>
+          <br />
+
+          <br />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Equipmentpage;
