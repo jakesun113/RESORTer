9c3cabd189a81bc497978db516dc409068318244
jakesun113 <jakesun113@gmail.com>
2018-09-27 15:52:11 +1000

    3.19.1 - change handle logout function

--- Frontend/src/components/GroupMemberPage/GroupMemberInfoCard.js
+++ Frontend/src/components/GroupMemberPage/GroupMemberInfoCard.js
@@ -116,15 +116,18 @@ class GroupMemberInfoCard extends Component {
       provider: null
     });
 
-    sessionStorage.removeItem("userSocialData");
-    sessionStorage.removeItem("userToken");
-    sessionStorage.removeItem("userImage");
-    sessionStorage.removeItem("userFinishProfile");
-    cookies.remove("user-name");
-    cookies.remove("access-token");
-    cookies.remove("user-pic");
-    cookies.remove("user-provider");
-    cookies.remove("user-profileFinished");
+      sessionStorage.removeItem("userSocialData");
+      sessionStorage.removeItem("userToken");
+      sessionStorage.removeItem("userImage");
+      sessionStorage.removeItem("userFinishProfile");
+      sessionStorage.removeItem("userFinishTrip");
+      sessionStorage.removeItem("userIsClicked");
+      cookies.remove("user-name");
+      cookies.remove("access-token");
+      cookies.remove("user-pic");
+      cookies.remove("user-provider");
+      cookies.remove("user-profileFinished");
+      cookies.remove("user-hasUnfinishedTrip");
   };
 
   render() {
