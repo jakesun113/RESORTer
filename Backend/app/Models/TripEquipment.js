98b2c9374e42b09fa5633e349cfbc5bca7aa8c08
SeaskyHo <hehaitian@SeaskyMAC.local>
2018-10-03 14:59:08 +1000

    database add tables

---
+++ Backend/app/Models/TripEquipment.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const Model = use('Model')
+
+class TripEquipment extends Model {
+}
+
+module.exports = TripEquipment
---
+++ Backend/database/migrations/1538535066583_trip_equipment_schema.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class TripEquipmentSchema extends Schema {
+  up() {
+    this.create('trip_equipments', (table) => {
+      table.increments()
+      table.timestamps()
+      table.integer('TripID').unsigned().notNullable().references('id').inTable('trips')
+      table.string('MemberType')
+      table.integer('MemberID')
+      table.mediumtext('RentInfo')
+      // table.boolean('SkiBoots')
+      // table.boolean('SkiPoles')
+      // table.string('SkiGrade', 30)
+      // table.boolean('SnowboardBoots')
+      // table.boolean('SnowboardBoard')
+      // table.string('SnowboardGrade', 30)
+      // table.boolean('TelemarkBoots')
+      // table.boolean('TelemarkPoles')
+      // table.string('TelemarkGrade', 30)
+      // table.date('Date')
+      // table.string('Duration', 20)
+      table.string('Outfit', 30)
+      table.string('Helmet', 30)
+      table.smallint('ShoeSize')
+      table.smallint('Height')
+      table.smallint('Weight')
+    })
+  }
+
+  down() {
+    this.drop('trip_equipments')
+  }
+}
+
+module.exports = TripEquipmentSchema
