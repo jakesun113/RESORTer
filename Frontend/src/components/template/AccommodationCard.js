386b22f38a2fa681457829176844d5a0f0a41bbe
yunjie Jia <453773874@qq.com>
2018-10-03 14:41:32 +1000

    fix accomodation card

--- Frontend/src/components/template/AccommodationCard.js
+++ Frontend/src/components/template/AccommodationCard.js
@@ -2,25 +2,87 @@ import React, { Component } from "react";
 
 class AccommodationCard extends Component {
   render() {
+    const { accommodation } = this.props;
     return (
       <React.Fragment>
-        <tbody
-          style={{
-            border: "1px solid rgb(232, 234, 237)",
-            height: "auto",
-            boxShadow: "2px 3px rgb(232, 234, 237)"
-          }}
-        >
-          <tr>
-            <td>{this.props.type}</td>
-            <td>{this.props.category}</td>
-            <td>{this.props.adultNum}</td>
-            <td>{this.props.childNum}</td>
-            <td>{this.props.todNum}</td>
-            <td>{this.props.bedNum}</td>
-            <td>{this.props.bathNum}</td>
-          </tr>
-        </tbody>
+        {/* title */}
+        <div className="row" style={{ fontWeight: "bold" }}>
+          <div className="col-lg-1" />
+          <div
+            className="col-6 col-lg-2"
+            style={{ color: "black", fontSize: "23px" }}
+          >
+            Accommodation Needs
+          </div>
+          <div className="col-lg-7" />
+        </div>
+        {/* Accommodation list */}
+        <div className="row">
+          <div className="col-lg-1" />
+          <div className="col-12 col-lg-10">
+            <table className="table table-borderless">
+              <thead>
+                <tr style={{ color: "#686369" }}>
+                  <th scope="col">Type</th>
+                  <th scope="col">Category</th>
+                  <th scope="col"># of adults</th>
+                  <th scope="col"># of children</th>
+                  <th scope="col"># of toddlers</th>
+                  <th scope="col"># of bedroom</th>
+                  <th scope="col"># of bathroom</th>
+                </tr>
+              </thead>
+
+              <tbody />
+              {accommodation.map(eachAccommodation => (
+                <tr
+                  style={{
+                    color: "#4682B3"
+                  }}
+                >
+                  <td>{eachAccommodation.type}</td>
+                  <td>{eachAccommodation.category}</td>
+                  <td>{eachAccommodation.adultNum}</td>
+                  <td>{eachAccommodation.childNum}</td>
+                  <td>{eachAccommodation.todNum}</td>
+                  <td>{eachAccommodation.bedNum}</td>
+                  <td>{eachAccommodation.bathNum}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+          <div className="col-lg-1" />
+        </div>
+        {/* Requests */}
+        <div className="row">
+          <div className="col-lg-1" />
+          <div className="col-12 col-lg-10">
+            <table className="table table-borderless">
+              <thead>
+                <tr style={{ color: "#686369" }}>
+                  <th scope="col">Specific Requests:</th>
+                </tr>
+              </thead>
+
+              <tbody />
+              <tr
+                style={{
+                  color: "#4682B3"
+                }}
+              >
+                <td>
+                  <textarea
+                    style={this.props.style}
+                    readOnly={this.props.readOnly}
+                  >
+                    Sb jiacheng book a room
+                  </textarea>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div className="col-lg-1" />
+        </div>
       </React.Fragment>
     );
   }
